382440c84d4a0042ef01bd18567c08ff22d8efc3
JVNunes50 <jvnunes1944@gmail.com>
2025-07-27 17:19:50 -0300

    Verificando se usuário está logado

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,7 @@
 |
 */
 
+use App\Http\Controllers\DepartamentoControlador;
 use App\Http\Controllers\ProdutoControlador;
 
 Route::get('/', function () {
@@ -22,3 +23,5 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/produtos', 'ProdutoControlador@index')->name('produtos');
+
+Route::get('/departamentos', 'DepartamentoControlador@index')->name('departamentos');
